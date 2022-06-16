ea7d5aecec3060e5efb350a48eee948d4cfad82e
Monalisa Menezes <psimonafmenezes@gmail.com>
2022-06-16 20:12:11 -0300

    feat: create next-auth

--- src/components/ButtonSignIn/index.tsx
+++ src/components/ButtonSignIn/index.tsx
@@ -1,22 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
-import styles from './styles.module.scss'
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
 export const ButtonSignIn = () => {
-  const session = true
+  const [session] = useSession()
 
   return session ? (
-    <button type='button' className={styles.ButtonSignIn} onClick={() => {}}>
-        <img src="https://joeschmoe.io/api/v1/random" alt="Foto do usuário" />
-        Olá Monalisa
-        <FiX color='#737380' className={styles.closeIcon}/>
-     
+    <button type="button" className={styles.ButtonSignIn} onClick={() => signOut()}>
+      <img src="https://joeschmoe.io/api/v1/random" alt="Foto do usuário" />
+      Olá Monalisa
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.ButtonSignIn} onClick={() => {}}>
-      <FaGithub color='#FFB800' />
+    <button
+      type="button"
+      className={styles.ButtonSignIn}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#FFB800" />
       Entrar com github
     </button>
-  )
-}
+  );
+};
